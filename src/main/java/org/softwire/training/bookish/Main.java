5b794d259ca28f3d331b9dc0ed89934d9a77e2b9
gparle <gabriel.j.parle@aexp.com>
2022-09-27 15:37:21 +0100

    done both jdbi and jdbc

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -2,9 +2,8 @@ package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 
 public class Main {
@@ -12,8 +11,8 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
         String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String user = "gparle";
+        String password = "gparle";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
@@ -27,9 +26,23 @@ public class Main {
         // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
 
         Connection connection = DriverManager.getConnection(connectionString);
+        String query = "SELECT * FROM bookish.Books";
+        try {
+            Statement stmt = connection.createStatement();
 
-
-
+        ResultSet rs = stmt.executeQuery(query);
+        System.out.println(rs);
+        while (rs.next()) {
+            String title = rs.getString("title");
+//            int supplierID = rs.getInt("SUP_ID");
+//            float price = rs.getFloat("PRICE");
+//            int sales = rs.getInt("SALES");
+//            int total = rs.getInt("TOTAL");
+            System.out.println(title);
+        }
+        }catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 
     private static void jdbiMethod(String connectionString) {
@@ -40,6 +53,15 @@ public class Main {
         // Use the "Book" class that we've created for you (in the models.database folder)
 
         Jdbi jdbi = Jdbi.create(connectionString);
+
+        List<Book> users = jdbi.withHandle(handle -> {
+
+            return handle.createQuery("SELECT * FROM bookish.Books")
+                    .mapToBean(Book.class)
+                    .list();
+        });
+
+        System.out.println(users.get(0).getAuthor());
 
 
 
