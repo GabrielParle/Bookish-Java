68a88339f5690e1b57457f435942f8f6c586c4fe
tim-barton96 <tbart@xwt34pl7nl.uk.aexp.com>
2022-09-27 15:38:10 +0100

    skeleton of database

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -1,10 +1,10 @@
 package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
+import org.softwire.training.bookish.models.database.Book;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.List;
 
 
 public class Main {
@@ -12,8 +12,8 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         String hostname = "localhost";
         String database = "bookish";
-        String user = "bookish";
-        String password = "bookish";
+        String user = "root";
+        String password = " ";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
         jdbcMethod(connectionString);
@@ -21,27 +21,37 @@ public class Main {
     }
 
     private static void jdbcMethod(String connectionString) throws SQLException {
-        System.out.println("JDBC method...");
-
-        // TODO: print out the details of all the books (using JDBC)
-        // See this page for details: https://docs.oracle.com/javase/tutorial/jdbc/basics/processingsqlstatements.html
 
         Connection connection = DriverManager.getConnection(connectionString);
-
+        String query = "select * from books";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                String title = resultSet.getString("Title");
+                System.out.println(title);
+            }
+        }
 
 
     }
 
     private static void jdbiMethod(String connectionString) {
-        System.out.println("\nJDBI method...");
+
 
         // TODO: print out the details of all the books (using JDBI)
         // See this page for details: http://jdbi.org
         // Use the "Book" class that we've created for you (in the models.database folder)
 
         Jdbi jdbi = Jdbi.create(connectionString);
+        List<Book> books = jdbi.withHandle(handle -> {
+            return handle.createQuery("SELECT * FROM books ORDER BY BookID")
+                    .mapToBean(Book.class)
+                    .list();
 
+        });
 
+        books.stream()
+                .forEach(book -> System.out.println(book.getAuthor()));
 
     }
 }
--- src/main/java/org/softwire/training/bookish/models/database/Book.java
+++ src/main/java/org/softwire/training/bookish/models/database/Book.java
@@ -2,4 +2,60 @@ package org.softwire.training.bookish.models.database;
 
 public class Book {
 
+    private int BookId;
+    private String Title;
+    private String Author;
+    private String ISBN;
+    private String Category;
+    private int copies;
+
+    public Book() {}
+
+    public int getBookId() {
+        return BookId;
+    }
+
+    public void setBookId(int bookId) {
+        BookId = bookId;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public void setCategory(String category) {
+        Category = category;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
 }
